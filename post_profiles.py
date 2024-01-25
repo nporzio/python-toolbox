b3fbfd51a194d3d1c8858d6240bc4f47b455157b
nporzio <30311249+nporzio@users.noreply.github.com>
2024-01-25 11:17:25 +0100

    Create post_profiles.py

---
+++ post_profiles.py
@@ -0,0 +1,55 @@
+import sys
+import json
+import requests
+import faker
+import hashlib
+from ratelimit import limits, sleep_and_retry
+
+
+print(sys.version)
+
+# random email generator // f.email()
+f = faker.Faker()
+
+@sleep_and_retry
+@limits(calls=100, period=1)
+def api_call(identifier, url, payload):
+
+	r_session = requests.Session()
+
+	user = "python.testuser@relay42.com"
+	password = "8D7UCMfn%EIy4@l!2tmCOfJw"
+	headers = {
+		"Content-Type": "application/json",
+		"accept": "*/*"
+	}
+
+
+	r = r_session.post(
+		url = url+identifier,
+		auth = (user, password),
+		headers = headers,
+		json = payload
+	)
+	
+	if r.status_code not in [200,201]:
+		raise(r.text)
+	else:
+		print(r.status_code)
+
+
+
+url = "https://api.relay42.com/v1/site-1268/profiles/7001/facts?forceInsert=false&partnerId="
+
+data = [{
+	"factName": "test",
+	"factTtl": 99999999,
+	"properties": {}
+	}]
+
+for i in range(1000):
+
+	dummy_email = f.email()
+	sha256_email = hashlib.sha256(dummy_email.encode('utf-8')).hexdigest()
+
+	api_call(identifier=sha256_email, url=url, payload=data)
